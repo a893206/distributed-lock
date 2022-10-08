b0153037218fcc2a938c331545fde232172fbaee
CR <931009686@qq.com>
2022-10-08 12:29:26 +0800

    feat: init

---
+++ src/test/java/com/cr/distributed/lock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cr.distributed.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
